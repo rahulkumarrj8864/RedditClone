78d25d238cb09d0ca3d07ca03d637b4720bd6fc6
rahulkumarrj8864 <rahulkumar8864084963@gmail.com>
2022-03-07 23:48:20 +0530

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+function upvote()
+{
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML); 
+    count++; 
+    votes.innerHTML = count;   
+}
+
+function downvote()
+{
+    let votes = document.getElementById('votes');
+    let count = parseInt(votes.innerHTML); 
+    count--; 
+    votes.innerHTML = count;   
+}
